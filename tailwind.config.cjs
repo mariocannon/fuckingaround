cc3e143cb72127401fc1f7186d9cd3b51994e6ba
mariocannon <cannons.for.u@gmail.com>
2024-09-24 22:34:45 +1200

    ds

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
-
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('flowbite/plugin')
+	],
+}
